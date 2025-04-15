944c02fef5ed88f27c34cd556944047c2f3974cb
letrongthuc <letrongthuc23042005@gmail.com>
2025-04-16 06:34:32 +0700

    log API base URL for debugging

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,9 @@
 // src/utils/axios.js
 import axios from 'axios';
 
+// Kiem tra
+console.log('API Base URL:', import.meta.env.VITE_API_BASE_URL);
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL, 
   headers: {
